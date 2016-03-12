a0649b6f7c7df0aed74995efda6caa3f798e8ffc
Rob Garrison <wowmotty@gmail.com>
2016-03-12 08:46:09 -0600

    version bump

--- github-custom-emojis.user.js
+++ github-custom-emojis.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GitHub Custom Emojis
-// @version      0.1.6
+// @version      0.1.7
 // @description  Add custom emojis from json source
 // @namespace    https://github.com/StylishThemes
 // @include      /https?://((gist)\.)?github\.com/
